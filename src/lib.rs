a23f40d0f3e3208dafb458eb440dd93072abb2ac
Naja Melan <najamelan@autistici.org>
2020-03-16 15:41:02 +0100

    Beef up docs configuration.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //
 #![cfg_attr( feature = "docs", feature(doc_cfg, external_doc) )]
 #![cfg_attr( feature = "docs", doc(include = "../README.md")  )]
-//!
+#![doc = ""] // empty doc line to handle missing doc warning when the feature is missing.
 
 
 #![ doc    ( html_root_url = "https://docs.rs/ws_stream_io" ) ]
